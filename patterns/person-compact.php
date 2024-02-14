dda3a833be871de0d84559d55b53050f9aa2d885
Greg <gd.temp@pm.me>
2024-02-14 14:52:18 +0100

    [feature] added four more patterns

---
+++ patterns/person-compact.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: PersonCompact
+ * Slug: theme_test/personCompact
+ * Categories: featured
+ */
+?>
+<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+    <p>marcel.salathe@epfl.ch</p>
+    <!-- /wp:paragraph -->
+    
+    <!-- wp:paragraph -->
+    <p>+41 21 693 09 91</p>
+    <!-- /wp:paragraph --></div>
+    <!-- /wp:group -->

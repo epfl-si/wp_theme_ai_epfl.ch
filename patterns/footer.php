43d11fa233b94d9b512d9526f962bf8639e3453e
Greg <gd.temp@pm.me>
2024-02-14 14:42:03 +0100

    [feature] added a bunch of new files to start making patterns in order to be customizable

---
+++ patterns/footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Title: Footer
+ * Slug: theme_test/footer
+ * Categories: featured
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"2rem","right":"2rem","bottom":"2rem","left":"2rem"}}}} -->
+<div class="wp-block-group" style="padding-top:2rem;padding-right:2rem;padding-bottom:2rem;padding-left:2rem">
+
+	<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+	<div class="wp-block-group alignwide">
+
+        <!-- wp:template-part {"slug":"epfl-logo"} /-->
+        <!-- wp:template-part {"slug":"footer-contact"} /-->
+        <!-- wp:template-part {"slug":"footer-nav"} /-->
+        <!-- wp:template-part {"slug":"menu-social"} /-->
+        <!-- wp:template-part {"slug":"legal-links"} /-->
+        <!-- wp:template-part {"slug":"copyright"} /-->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
+
+
---
+++ patterns/header.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: Header
+ * Slug: theme_test/header
+ * Categories: featured
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"},"margin":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--30);margin-bottom:var(--wp--preset--spacing--30);padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:template-part {"slug":"epfl-logo","theme":"theme_test"} /-->
+
+<!-- wp:template-part {"slug":"search-bar","theme":"theme_test"} /-->
+
+<!-- wp:template-part {"slug":"touch-menu-nav","theme":"theme_test"} /--></div>
+<!-- /wp:group -->

3125cf13e771b7eddd15144521d5dc786c3dbf3e
Greg <gd.temp@pm.me>
2024-02-22 15:05:45 +0100

    [feature] add CSS class to the list to make the separators [feature] add a new file to handle the hero pattern for dark theme

---
+++ patterns/hero_dark_theme.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: HeroDarkTheme
+ * Slug: theme_ai_center/heroDarkTheme
+ * Categories: featured, theme_ai_center/ai-center
+ */
+?>
+<!-- wp:cover {"overlayColor":"grey_900","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover"><span aria-hidden="true" class="wp-block-cover__background has-grey-900-background-color has-background-dim-100 has-background-dim"></span>
+    <div class="wp-block-cover__inner-container">
+        <!-- wp:heading -->
+        <h2 class="wp-block-heading">Exploring AI to Advance Research, Industry, and Education</h2>
+        <!-- /wp:heading -->
+    </div>
+</div>
+<!-- /wp:cover -->

6d52dd9660af421578113551be6dd35094253a02
Greg <gd.temp@pm.me>
2024-02-22 15:35:01 +0100

    [feature] add the hero cover dark theme on the posts template

--- patterns/hero_dark_theme.php
+++ patterns/hero_dark_theme.php
@@ -8,9 +8,9 @@
 <!-- wp:cover {"overlayColor":"grey_900","layout":{"type":"constrained"}} -->
 <div class="wp-block-cover"><span aria-hidden="true" class="wp-block-cover__background has-grey-900-background-color has-background-dim-100 has-background-dim"></span>
     <div class="wp-block-cover__inner-container">
-        <!-- wp:heading -->
-        <h2 class="wp-block-heading">Exploring AI to Advance Research, Industry, and Education</h2>
-        <!-- /wp:heading -->
+      
+        <!-- wp:post-title /-->
+
     </div>
 </div>
 <!-- /wp:cover -->

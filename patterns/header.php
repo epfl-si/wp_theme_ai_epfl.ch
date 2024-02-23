f676b2ac896e20312d4c2b35249cd1063c1b1f1b
Greg <gd.temp@pm.me>
2024-02-23 09:19:15 +0100

    [fix] change the logo name to match the "ai center" logo and not "epfl" logo

--- patterns/header.php
+++ patterns/header.php
@@ -7,7 +7,7 @@
 ?>
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"},"margin":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
 <div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--30);margin-bottom:var(--wp--preset--spacing--30);padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)">
-    <!-- wp:template-part {"slug":"epfl-logo","theme":"theme_ai_center"} /-->
+    <!-- wp:template-part {"slug":"logo","theme":"theme_ai_center"} /-->
 
     <!-- wp:template-part {"slug":"search-bar","theme":"theme_ai_center"} /-->
 

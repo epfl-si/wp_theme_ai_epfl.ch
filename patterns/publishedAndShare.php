bc9275f333bd02bda1574e8791b80a583616eb78
Greg <gd.temp@pm.me>
2024-02-23 16:16:27 +0100

    [feature] getting closer of Jason's style for the posts. Add some divsions with divs to contain other div and respect Jason's structure

--- patterns/publishedAndShare.php
+++ patterns/publishedAndShare.php
@@ -5,42 +5,40 @@
  * Categories: featured, theme_ai_center/ai-center
  */
 ?>
-<!-- wp:group {"style":{"layout":{"selfStretch":"fixed","flexSize":"25%"}},"layout":{"type":"constrained"}, "tagName": "aside"} -->
-<aside class="wp-block-group">
+
+<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group col-sm-12 col-md-4 col-3 article-details">
     <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
-    <div class="wp-block-group col-3 article-details">
+    <div class="wp-block-group sub-container-sm">
+        
+        <!-- wp:heading {"level":6} -->
+        <h6 class="wp-block-heading h6">Published</h6>
+        <!-- /wp:heading -->
+
+        <!-- wp:paragraph -->
+        <p>11 December, 2023</p>
+        <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+    <div class="wp-block-group">
         <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
         <div class="wp-block-group sub-container-sm">
             
             <!-- wp:heading {"level":6} -->
-            <h6 class="wp-block-heading h6">Published</h6>
+            <h6 class="wp-block-heading h6">Share</h6>
             <!-- /wp:heading -->
 
-            <!-- wp:paragraph -->
-            <p>11 December, 2023</p>
-            <!-- /wp:paragraph -->
-        </div>
-        <!-- /wp:group -->
-
-        <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
-        <div class="wp-block-group">
-            <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
-            <div class="wp-block-group sub-container-sm">
-                
-                <!-- wp:heading {"level":6} -->
-                <h6 class="wp-block-heading h6">Share</h6>
-                <!-- /wp:heading -->
-
-                <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-                <div class="wp-block-group">
-                    <!-- wp:embed {"providerNameSlug":"twitter","responsive":true} /-->
-                </div>
-                <!-- /wp:group -->
-              </div>
+            <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+            <div class="wp-block-group">
+                <!-- wp:embed {"providerNameSlug":"twitter","responsive":true} /-->
+            </div>
             <!-- /wp:group -->
-        </div>
+            </div>
         <!-- /wp:group -->
     </div>
     <!-- /wp:group -->
-</aside>
+</div>
 <!-- /wp:group -->
+
--- patterns/research-topics.php
+++ patterns/research-topics.php
@@ -5,34 +5,38 @@
  * Categories: featured, theme_ai_center/ai-center
  */
 ?>
-<!-- wp:group {"style":{"layout":{"selfStretch":"fixed","flexSize":"25%"}},"layout":{"type":"flex","orientation":"vertical"}} -->
-<div class="wp-block-group col-3 sub-container-sm">
-    <!-- wp:heading {"level":6} -->
-    <h6 class="wp-block-heading h6">Research Topics</h6>
-    <!-- /wp:heading -->
-
-    <!-- wp:list -->
-    <ul class="divider-list">
-        <!-- wp:list-item -->
-        <li>Mathematical and Statistical Foundations of Machine Learning</li>
-        <!-- /wp:list-item -->
-
-        <!-- wp:list-item -->
-        <li>Interpretability and Exunstyledability</li>
-        <!-- /wp:list-item -->
-
-        <!-- wp:list-item -->
-        <li>Safety, Reliability, and Robustness</li>
-        <!-- /wp:list-item -->
-
-        <!-- wp:list-item -->
-        <li>Fairness and Bias of AI Systems</li>
-        <!-- /wp:list-item -->
-
-        <!-- wp:list-item -->
-        <li>Machine Perception</li>
-        <!-- /wp:list-item -->
-    </ul>
-    <!-- /wp:list -->
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group col-sm-12 col-md-8 col-3">
+    <!-- wp:group {"style":{"layout":{"selfStretch":"fixed","flexSize":"25%"}},"layout":{"type":"constrained"}} -->
+    <div class="wp-block-group sub-container-sm">
+        <!-- wp:heading {"level":6,"className":"h6"} -->
+        <h6 class="wp-block-heading h6">Research Topics</h6>
+        <!-- /wp:heading -->
+        
+        <!-- wp:list {"className":"divider-list"} -->
+        <ul class="divider-list">
+            <!-- wp:list-item -->
+            <li>Mathematical and Statistical Foundations of Machine Learning</li>
+            <!-- /wp:list-item -->
+            
+            <!-- wp:list-item -->
+            <li>Interpretability and Exunstyledability</li>
+            <!-- /wp:list-item -->
+            
+            <!-- wp:list-item -->
+            <li>Safety, Reliability, and Robustness</li>
+            <!-- /wp:list-item -->
+            
+            <!-- wp:list-item -->
+            <li>Fairness and Bias of AI Systems</li>
+            <!-- /wp:list-item -->
+            
+            <!-- wp:list-item -->
+            <li>Machine Perception</li>
+            <!-- /wp:list-item -->
+        </ul>
+        <!-- /wp:list -->
+    </div>
+    <!-- /wp:group -->
 </div>
 <!-- /wp:group -->

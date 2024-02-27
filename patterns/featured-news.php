50233f1981ce9e9de5c12e43c544aa9792601648
Ypso46 <gd.temp@pm.me>
2024-02-27 14:42:58 +0100

    [feature] the "news-featured" is now close to Jason's design

--- patterns/featured-news.php
+++ patterns/featured-news.php
@@ -6,13 +6,172 @@
  */
 ?>
 <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<div class="wp-block-group">
-    <!-- wp:template-part {"slug":"news-item-lg","theme":"theme_ai_center","area":"uncategorized"} /-->
-
+<div class="wp-block-group featured-news">
     <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-    <div class="wp-block-group">
-        <!-- wp:template-part {"slug":"row","theme":"theme_ai_center","area":"uncategorized"} /-->
+    <div class="wp-block-group center">
+        <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+        <div class="wp-block-group section-header">
+            <!-- wp:heading {"level":6} -->
+            <h6 class="wp-block-heading">News</h6>
+            <!-- /wp:heading -->
 
+            <!-- wp:group {"layout":{"type":"flex"}, "tagName":"a"} -->
+            <a class="wp-block-group link">
+                <span>Read all news</span>
+            </a>
+            <!-- /wp:group --> 
+        </div>
+        <!-- /wp:group -->
+
+        <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+        <div class="wp-block-group row">
+            <!-- wp:group -->
+            <div class="wp-block-group col-sm-12 col-6">
+                <a class="wp-block-group news-item-lg">
+                    <!-- wp:image -->
+                    <figure class="wp-block-image"><img src="http://localhost:8088/wp-content/uploads/2024/01/placeholder-news-1.jpg" alt="" class="wp-image-21"/></figure>
+                    <!-- /wp:image -->
+                    
+                    <!-- wp:heading {"level":3} -->
+                    <h3>A game changer for building robust distributed systems</h3>
+                    <!-- /wp:heading -->
+                    
+                    <!-- wp:paragraph -->
+                    <p>EPFL researchers have developed a new distributed algorithm that, for the first time, solves one of the key performance and reliability problems affecting most of the currently-deployed consensus protocols.</p>
+                    <!-- /wp:paragraph -->
+
+                    <!-- wp:group {"layout":{"type":"flex"}} -->
+                    <div class="wp-block-group link">
+                    <span>Read all news</span>
+                    </div>
+                    <!-- /wp:group --> 
+
+                </a>
+            </div>
+            <!-- /wp:group -->
+
+            <!-- wp:group -->
+            <div class="wp-block-group col-sm-12 col-3">
+                <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}, "tagName":"a"} -->
+                <a class="wp-block-group news-item">
+                    <!-- wp:heading {"level":5} -->
+                    <h5>A new EPFL tool shows the decline of political tone in the US</h5>
+                    <!-- /wp:heading -->
+                    
+                    <!-- wp:paragraph -->
+                    <p>A new EPFL developed tool, Quotebank, has helped researchers provide the first large-scale data-driven evidence...</p>
+                    <!-- /wp:paragraph -->
+
+                    <!-- wp:group {"layout":{"type":"flex"}} -->
+                    <div class="wp-block-group link-sm">
+                        <span>Read on epfl.ch</span>
+                    </div>
+                    <!-- /wp:group --> 
+
+                </a>
+                <!-- /wp:group -->
+
+                <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}, "tagName":"a"} -->
+                <a class="wp-block-group news-item">
+                    <!-- wp:heading {"level":5} -->
+                    <h5>A new EPFL tool shows the decline of political tone in the US</h5>
+                    <!-- /wp:heading -->
+                    
+                    <!-- wp:paragraph -->
+                    <p>A new EPFL developed tool, Quotebank, has helped researchers provide the first large-scale data-driven evidence...</p>
+                    <!-- /wp:paragraph -->
+
+                    <!-- wp:group {"layout":{"type":"flex"}} -->
+                    <div class="wp-block-group link-sm">
+                        <span>Read on epfl.ch</span>
+                    </div>
+                    <!-- /wp:group --> 
+
+                </a>
+                <!-- /wp:group -->
+
+                <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}, "tagName":"a"} -->
+                <a class="wp-block-group news-item">
+                    <!-- wp:heading {"level":5} -->
+                    <h5>A new EPFL tool shows the decline of political tone in the US</h5>
+                    <!-- /wp:heading -->
+                    
+                    <!-- wp:paragraph -->
+                    <p>A new EPFL developed tool, Quotebank, has helped researchers provide the first large-scale data-driven evidence...</p>
+                    <!-- /wp:paragraph -->
+
+                    <!-- wp:group {"layout":{"type":"flex"}} -->
+                    <div class="wp-block-group link-sm">
+                        <span>Read on epfl.ch</span>
+                    </div>
+                    <!-- /wp:group --> 
+
+                </a>
+                <!-- /wp:group -->
+            </div>
+            <!-- /wp:group -->
+            
+            <!-- wp:group -->
+            <div class="wp-block-group col-sm-12 col-3">
+                <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}, "tagName":"a"} -->
+                <a class="wp-block-group news-item">
+                    <!-- wp:heading {"level":5} -->
+                    <h5>A new EPFL tool shows the decline of political tone in the US</h5>
+                    <!-- /wp:heading -->
+                    
+                    <!-- wp:paragraph -->
+                    <p>A new EPFL developed tool, Quotebank, has helped researchers provide the first large-scale data-driven evidence...</p>
+                    <!-- /wp:paragraph -->
+
+                    <!-- wp:group {"layout":{"type":"flex"}} -->
+                    <div class="wp-block-group link-sm">
+                        <span>Read on epfl.ch</span>
+                    </div>
+                    <!-- /wp:group --> 
+
+                </a>
+                <!-- /wp:group -->
+
+                <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}, "tagName":"a"} -->
+                <a class="wp-block-group news-item">
+                    <!-- wp:heading {"level":5} -->
+                    <h5>A new EPFL tool shows the decline of political tone in the US</h5>
+                    <!-- /wp:heading -->
+                    
+                    <!-- wp:paragraph -->
+                    <p>A new EPFL developed tool, Quotebank, has helped researchers provide the first large-scale data-driven evidence...</p>
+                    <!-- /wp:paragraph -->
+
+                    <!-- wp:group {"layout":{"type":"flex"}} -->
+                    <div class="wp-block-group link-sm">
+                        <span>Read on epfl.ch</span>
+                    </div>
+                    <!-- /wp:group --> 
+
+                </a>
+                <!-- /wp:group -->
+
+                <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}, "tagName":"a"} -->
+                <a class="wp-block-group news-item">
+                    <!-- wp:heading {"level":5} -->
+                    <h5>A new EPFL tool shows the decline of political tone in the US</h5>
+                    <!-- /wp:heading -->
+                    
+                    <!-- wp:paragraph -->
+                    <p>A new EPFL developed tool, Quotebank, has helped researchers provide the first large-scale data-driven evidence...</p>
+                    <!-- /wp:paragraph -->
+
+                    <!-- wp:group {"layout":{"type":"flex"}} -->
+                    <div class="wp-block-group link-sm">
+                        <span>Read on epfl.ch</span>
+                    </div>
+                    <!-- /wp:group --> 
+                </a>
+                <!-- /wp:group -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:group -->
     </div>
     <!-- /wp:group -->
 </div>

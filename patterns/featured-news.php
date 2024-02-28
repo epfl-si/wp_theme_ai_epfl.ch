e4550976c8b87372217efec519d4f8a351a9d328
Ypso46 <gd.temp@pm.me>
2024-02-28 14:46:09 +0100

    [feature] DRY with the use of php functions [refactor] get rid of all WP's HTML comment

--- patterns/featured-news.php
+++ patterns/featured-news.php
@@ -9,79 +9,53 @@
 <?php
 function render_news_item_lg ($title, $image_url, $paragraph_html, $read_more_html) {
     ?>
+        <div class="col-sm-12 col-6">
+            <a class="news-item-lg">
 
-                <div class="wp-block-group col-sm-12 col-6">
-                <a class="wp-block-group news-item-lg">
+                <picture>
+                <img src="<?php echo($image_url); ?>" alt=""/>
+                </picture>
 
-                    <figure class="wp-block-image"><img src="<?php echo($image_url); ?>" alt="" class="wp-image-21"/></figure>
+                <h3><?php echo($title); ?></h3>
 
-                    
+                <?php echo($paragraph_html); ?>
 
-                    <h3><?php echo($title); ?></h3>
+                <div class="link">
+                <?php echo($read_more_html); ?>
+                </div>
 
-                    
-
-                    <?php echo($paragraph_html); ?>
-
-
-
-                    <div class="wp-block-group link">
-                    <?php echo($read_more_html); ?>
-                    </div>
-
-
-                </a>
-            </div>
-
-
+            </a>
+        </div>
     <?php
 }
 function render_news_item ($title, $paragraph_html, $read_more_html) {
-    ?>
-               
-                <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}, "tagName":"a"} -->
-                <a class="wp-block-group news-item">
-                    <!-- wp:heading {"level":5} -->
-                    <h5><?php echo($title); ?></h5>
-                    <!-- /wp:heading -->
-                    
-                    <!-- wp:paragraph -->
-                    <?php echo($paragraph_html); ?>
-                    <!-- /wp:paragraph -->
+    ?> 
+        <a class="news-item">
 
-                    <!-- wp:group {"layout":{"type":"flex"}} -->
-                    <div class="wp-block-group link-sm">
-                    <?php echo($read_more_html); ?>
-                    </div>
-                    <!-- /wp:group --> 
+            <h5><?php echo($title); ?></h5>
+            
+            <?php echo($paragraph_html); ?>
 
-                </a>
-                <!-- /wp:group -->
+            <div class="wp-block-group link-sm">
+            <?php echo($read_more_html); ?>
+            </div>
+        </a>
     <?php
 }
 
 ?>
 
-<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<div class="wp-block-group featured-news">
-    <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-    <div class="wp-block-group center">
-        <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-        <div class="wp-block-group section-header">
-            <!-- wp:heading {"level":6} -->
-            <h6 class="wp-block-heading">News</h6>
-            <!-- /wp:heading -->
 
-            <!-- wp:group {"layout":{"type":"flex"}, "tagName":"a"} -->
-            <a class="wp-block-group link">
+<div class="featured-news">
+    <div class="center">
+        <div class="section-header">
+            <h6>News</h6>
+            <a class="link">
                 <span>Read all news</span>
             </a>
-            <!-- /wp:group --> 
         </div>
-        <!-- /wp:group -->
 
-        <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-        <div class="wp-block-group row">
+        <div class="row">
             <?php
                 render_news_item_lg(
                     "A game changer for building robust distributed systems",
@@ -90,13 +64,14 @@ function render_news_item ($title, $paragraph_html, $read_more_html) {
                     "<span>Read all news</span>"
                 );
                 // TODO: replace with
-                if (false) {
-                    $posts = get_posts();
-                    render_news_item_lg($posts[0]);
-                }
+                // if (false) {
+                //   $posts = get_posts();
+                //    render_news_item_lg($posts[0]);
+                // }
+
              ?>
-            <!-- wp:group -->
-            <div class="wp-block-group col-sm-12 col-3">
+
+            <div class="col-sm-12 col-3">
                 <?php
                     render_news_item(
                         "A new EPFL tool shows the decline of political tone in the US",
@@ -115,70 +90,26 @@ function render_news_item ($title, $paragraph_html, $read_more_html) {
                     );
                 ?>
             </div>
-            <!-- /wp:group -->
-            
-            <!-- wp:group -->
-            <div class="wp-block-group col-sm-12 col-3">
-                <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}, "tagName":"a"} -->
-                <a class="wp-block-group news-item">
-                    <!-- wp:heading {"level":5} -->
-                    <h5>A new EPFL tool shows the decline of political tone in the US</h5>
-                    <!-- /wp:heading -->
-                    
-                    <!-- wp:paragraph -->
-                    <p>A new EPFL developed tool, Quotebank, has helped researchers provide the first large-scale data-driven evidence...</p>
-                    <!-- /wp:paragraph -->
 
-                    <!-- wp:group {"layout":{"type":"flex"}} -->
-                    <div class="wp-block-group link-sm">
-                        <span>Read on epfl.ch</span>
-                    </div>
-                    <!-- /wp:group --> 
-
-                </a>
-                <!-- /wp:group -->
-
-                <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}, "tagName":"a"} -->
-                <a class="wp-block-group news-item">
-                    <!-- wp:heading {"level":5} -->
-                    <h5>A new EPFL tool shows the decline of political tone in the US</h5>
-                    <!-- /wp:heading -->
-                    
-                    <!-- wp:paragraph -->
-                    <p>A new EPFL developed tool, Quotebank, has helped researchers provide the first large-scale data-driven evidence...</p>
-                    <!-- /wp:paragraph -->
-
-                    <!-- wp:group {"layout":{"type":"flex"}} -->
-                    <div class="wp-block-group link-sm">
-                        <span>Read on epfl.ch</span>
-                    </div>
-                    <!-- /wp:group --> 
-
-                </a>
-                <!-- /wp:group -->
-
-                <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}, "tagName":"a"} -->
-                <a class="wp-block-group news-item">
-                    <!-- wp:heading {"level":5} -->
-                    <h5>A new EPFL tool shows the decline of political tone in the US</h5>
-                    <!-- /wp:heading -->
-                    
-                    <!-- wp:paragraph -->
-                    <p>A new EPFL developed tool, Quotebank, has helped researchers provide the first large-scale data-driven evidence...</p>
-                    <!-- /wp:paragraph -->
-
-                    <!-- wp:group {"layout":{"type":"flex"}} -->
-                    <div class="wp-block-group link-sm">
-                        <span>Read on epfl.ch</span>
-                    </div>
-                    <!-- /wp:group --> 
-                </a>
-                <!-- /wp:group -->
+            <div class="col-sm-12 col-3">
+                <?php
+                    render_news_item(
+                        "A new EPFL tool shows the decline of political tone in the US",
+                        "<p>A new EPFL developed tool, Quotebank, has helped researchers provide the first large-scale data-driven evidence...</p>",
+                        "<span>Read on epfl.ch</span>"
+                    );
+                    render_news_item(
+                        "AMLD EPFL 2024 call for workshops",
+                        "<p>A new EPFL developed tool, Quotebank, has helped researchers provide the first large-scale data-driven evidence...</p>",
+                        "<span>Read on appliedmldays.org</span>"
+                    );
+                    render_news_item(
+                        "A new EPFL tool shows the decline of political tone in the US",
+                        "<p>A new EPFL developed tool, Quotebank, has helped researchers provide the first large-scale data-driven evidence...</p>",
+                        "<span>Read on epfl.ch</span>"
+                    );
+                ?>
             </div>
-            <!-- /wp:group -->
         </div>
-        <!-- /wp:group -->
     </div>
-    <!-- /wp:group -->
 </div>
-<!-- /wp:group -->

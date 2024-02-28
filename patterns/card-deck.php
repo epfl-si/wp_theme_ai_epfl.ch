eb2f3121b60e900e3448d2fe286cde38b4d3c51a
Ypso46 <gd.temp@pm.me>
2024-02-28 14:23:14 +0100

    [feature] card-deck: styling according to Jason's design [feature] index.html: add the pattern part instead of the template part

--- patterns/card-deck.php
+++ patterns/card-deck.php
@@ -6,107 +6,148 @@
  */
 ?>
 <!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
-<div class="wp-block-group">
+<div class="wp-block-group card-deck">
     <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-    <div class="wp-block-group">
-        <!-- wp:paragraph -->
-        <p>Events</p>
-        <!-- /wp:paragraph -->
+    <div class="wp-block-group center">
+        <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+        <div class="wp-block-group section-header">
+            <!-- wp:heading {"level":6} -->
+            <h6 class="wp-block-heading">News</h6>
+            <!-- /wp:heading -->
+
+            <!-- wp:group {"layout":{"type":"flex"}, "tagName":"a"} -->
+            <a class="wp-block-group link">
+                <span>Read all news</span>
+            </a>
+            <!-- /wp:group --> 
+        </div>
+        <!-- /wp:group -->
     
-        <!-- wp:paragraph {"align":"left"} -->
-        <p class="has-text-align-left">See all events</p>
-        <!-- /wp:paragraph -->
-    </div>
-    <!-- /wp:group -->
         
-    <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-    <div class="wp-block-group">
-        <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
-        <div class="wp-block-group">
-            <!-- wp:image {"id":18,"sizeSlug":"full","linkDestination":"none"} -->
-            <figure class="wp-block-image size-full"><img src="http://localhost:8088/wp-content/uploads/2024/01/placeholder-event-1.jpg" alt="" class="wp-image-18"/></figure>
-            <!-- /wp:image -->
-    
-            <!-- wp:heading -->
-            <h2 class="wp-block-heading">AMLD EPFL 2024</h2>
-            <!-- /wp:heading -->
+        <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+        <div class="wp-block-group row">
+            <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+            <div class="wp-block-group col-sm-12 col-4">
+                <a class="wp-block-group event" href="#">
+                    <!-- wp:image {"id":18,"sizeSlug":"full","linkDestination":"none"} -->
+                    <figure class="wp-block-image"><img src="http://localhost:8088/wp-content/uploads/2024/01/placeholder-event-1.jpg" alt="" class="wp-image-18"/></figure>
+                    <!-- /wp:image -->
             
-            <!-- wp:post-date /-->
-            
-            <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-            <div class="wp-block-group">
-                <!-- wp:paragraph -->
-                <p>09:00-19:00</p>
-                <!-- /wp:paragraph -->
-            
-                <!-- wp:paragraph -->
-                <p>EPFL, Lausanne</p>
-                <!-- /wp:paragraph -->
+                    <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+                    <div class="wp-block-group event-content">
+                        <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+                        <div class="wp-block-group">
+                            <header>
+                            <!-- wp:heading {"level":4} -->
+                            <h4>AMLD EPFL 2024</h4><br>
+                            <span class="event-date red">23-26 Mar. 2024</span>
+                            <!-- /wp:heading -->
+                                
+                            <!-- wp:list -->
+                            <ul class="event-time-location">
+                                <!-- wp:list-item -->
+                                <li class="event-time"><span>09:00-19:00</span></li>
+                                <!-- /wp:list-item -->
+                                <!-- wp:list-item -->
+                                <li class="event-location"><span>EPFL, Lausanne</span></li>
+                                <!-- /wp:list-item -->
+                            </ul>
+                            <!-- /wp:list -->
+                        </header>
+                        </div>
+                        <!-- /wp:group -->  
+
+                        <!-- wp:group {"layout":{"type":"flex"}} -->
+				        <div class="wp-block-group link-sm">
+                            <span>Visit appliedmldays.org</span>
+                        </div>
+                        <!-- /wp:group -->
+                    </div>
+                    <!-- /wp:group -->  
+                </a>
             </div>
             <!-- /wp:group -->
-                
-            <!-- wp:paragraph -->
-            <p>Visit appliedmldays.org</p>
-            <!-- /wp:paragraph -->
-        </div>
-        <!-- /wp:group -->
-    
-        <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
-        <div class="wp-block-group">
-            <!-- wp:image {"id":19,"sizeSlug":"full","linkDestination":"none"} -->
-            <figure class="wp-block-image size-full"><img src="http://localhost:8088/wp-content/uploads/2024/01/placeholder-event-2.jpg" alt="" class="wp-image-19"/></figure>
-            <!-- /wp:image -->
+        
+            <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+            <div class="wp-block-group col-sm-12 col-4">
+                <a class="wp-block-group event" href="#">
+                    <!-- wp:image {"id":18,"sizeSlug":"full","linkDestination":"none"} -->
+                    <figure class="wp-block-image"><img src="http://localhost:8088/wp-content/uploads/2024/01/placeholder-event-2.jpg" alt="" class="wp-image-18"/></figure>
+                    <!-- /wp:image -->
             
-            <!-- wp:heading {"style":{"layout":{"selfStretch":"fit","flexSize":null}}} -->
-            <h2 class="wp-block-heading">Industry Research Workshop: Machine Learning for Health Care and Life Sciences</h2>
-            <!-- /wp:heading -->
-            
-            <!-- wp:post-date /-->
-            
-            <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-            <div class="wp-block-group">
-                <!-- wp:paragraph -->
-                <p>09:00-19:00</p>
-                <!-- /wp:paragraph -->
-                
-                <!-- wp:paragraph -->
-                <p>EPFL, Lausanne</p>
-                <!-- /wp:paragraph -->
+                    <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+                    <div class="wp-block-group event-content">
+                        <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+                        <div class="wp-block-group">
+                            <!-- wp:heading {"level":4} -->
+                            <h4>Industry Research Workshop: Machine Learning for Health Care and Life Sciences</h4><br>
+                            <span class="event-date red">23-26 Dec. 2024</span>
+                            <!-- /wp:heading -->
+                                
+                            <!-- wp:list -->
+                            <ul class="event-time-location">
+                                <!-- wp:list-item -->
+                                <li class="event-time"><span>09:00-19:00</span></li>
+                                <!-- /wp:list-item -->
+                                <!-- wp:list-item -->
+                                <li class="event-location"><span>EPFL, Lausanne</span></li>
+                                <!-- /wp:list-item -->
+                            </ul>
+                            <!-- /wp:list -->
+                        </div>
+                        <!-- /wp:group -->
+
+                        <!-- wp:group {"layout":{"type":"flex"}} -->
+				        <div class="wp-block-group link-sm">
+                            <span>Visit epfl.ch</span>
+                        </div>
+                        <!-- /wp:group -->
+                    </div>
+                    <!-- /wp:group -->  
+                </a>
             </div>
             <!-- /wp:group -->
+        
+            <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+            <div class="wp-block-group col-sm-12 col-4">
+                <a class="wp-block-group event" href="#">
+                    <!-- wp:image {"id":18,"sizeSlug":"full","linkDestination":"none"} -->
+                    <figure class="wp-block-image"><img src="http://localhost:8088/wp-content/uploads/2024/01/placeholder-event-3.jpg" alt="" class="wp-image-18"/></figure>
+                    <!-- /wp:image -->
             
-            <!-- wp:paragraph -->
-            <p>Visit appliedmldays.org</p>
-            <!-- /wp:paragraph -->
-        </div>
-        <!-- /wp:group -->
-    
-        <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
-        <div class="wp-block-group"><!-- wp:image {"id":20,"sizeSlug":"full","linkDestination":"none"} -->
-            <figure class="wp-block-image size-full"><img src="http://localhost:8088/wp-content/uploads/2024/01/placeholder-event-3.jpg" alt="" class="wp-image-20"/></figure>
-            <!-- /wp:image -->
-            
-            <!-- wp:heading -->
-            <h2 class="wp-block-heading">Python for Data Science and Machine Learning (EN) 2023</h2>
-            <!-- /wp:heading -->
-            
-            <!-- wp:post-date /-->
-            
-            <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-            <div class="wp-block-group">
-                <!-- wp:paragraph -->
-                <p>09:00-19:00</p>
-                <!-- /wp:paragraph -->
-                
-                <!-- wp:paragraph -->
-                <p>EPFL, Lausanne</p>
-                <!-- /wp:paragraph -->
+                    <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+                    <div class="wp-block-group event-content">
+                        <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+                        <div class="wp-block-group">
+                            <!-- wp:heading {"level":4} -->
+                            <h4>Python for Data Science and Machine Learning (EN) 2023</h4><br>
+                            <span class="event-date red">23-26 Mar. 2024</span>
+                            <!-- /wp:heading -->
+                                
+                            <!-- wp:list -->
+                            <ul class="event-time-location">
+                                <!-- wp:list-item -->
+                                <li class="event-time"><span>09:00-19:00</span></li>
+                                <!-- /wp:list-item -->
+                                <!-- wp:list-item -->
+                                <li class="event-location"><span>EPFL, Lausanne</span></li>
+                                <!-- /wp:list-item -->
+                            </ul>
+                            <!-- /wp:list -->
+                        </div>
+                        <!-- /wp:group --> 
+
+                        <!-- wp:group {"layout":{"type":"flex"}} -->
+				        <div class="wp-block-group link-sm">
+                            <span>Visit epfl.ch</span>
+                        </div>
+                        <!-- /wp:group -->
+                    </div>
+                    <!-- /wp:group -->  
+                </a>
             </div>
             <!-- /wp:group -->
-            
-            <!-- wp:paragraph -->
-            <p>Visit appliedmldays.org</p>
-            <!-- /wp:paragraph -->
+
         </div>
         <!-- /wp:group -->
     </div>

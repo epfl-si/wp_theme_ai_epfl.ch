62a671981e02ef6b469a0885d98a3fd6c33a7e05
Ypso46 <gd.temp@pm.me>
2024-02-29 08:56:54 +0100

    [feature] the clock and location icons are now integrated

--- patterns/card-deck.php
+++ patterns/card-deck.php
@@ -5,152 +5,81 @@
  * Categories: featured, theme_ai_center/ai-center
  */
 ?>
-<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
-<div class="wp-block-group card-deck">
-    <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-    <div class="wp-block-group center">
-        <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-        <div class="wp-block-group section-header">
-            <!-- wp:heading {"level":6} -->
-            <h6 class="wp-block-heading">News</h6>
-            <!-- /wp:heading -->
 
-            <!-- wp:group {"layout":{"type":"flex"}, "tagName":"a"} -->
+<?php
+function render_event_item ($image_url, $title, $date, $time, $location, $read_more_html) {
+    ?>
+        <div class="col-sm-12 col-4">
+            <a class="event" href="#">
+                <picture><img src="<?php echo($image_url); ?>" alt=""/></picture>
+
+                <div class="event-content">
+                    <header>
+                        <h4>
+                            <?php echo($title); ?><br>
+                            <span class="event-date red"><?php echo($date); ?></span>
+                        </h4>    
+                        <ul class="event-time-location">
+                            <li class="event-time">
+                                <i class="ph ph-clock"></i>
+                                <span><?php echo($time); ?></span>
+                            </li>
+                            <li class="event-location">
+                                <i class="ph ph-map-pin"></i>
+                                <span><?php echo($location); ?></span>
+                            </li>
+                        </ul>
+                    </header>
+                    <div class="wp-block-group link-sm">
+                        <span><?php echo($read_more_html); ?></span>
+                    </div>
+                </div>
+            </a>
+        </div>
+    <?php 
+}
+
+?>
+
+
+<div class="card-deck">
+    <div class="center">
+        <div class="section-header">
+            <h6>Events</h6>
             <a class="wp-block-group link">
                 <span>Read all news</span>
             </a>
-            <!-- /wp:group --> 
         </div>
-        <!-- /wp:group -->
-    
-        
-        <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-        <div class="wp-block-group row">
-            <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
-            <div class="wp-block-group col-sm-12 col-4">
-                <a class="wp-block-group event" href="#">
-                    <!-- wp:image {"id":18,"sizeSlug":"full","linkDestination":"none"} -->
-                    <figure class="wp-block-image"><img src="http://localhost:8088/wp-content/uploads/2024/01/placeholder-event-1.jpg" alt="" class="wp-image-18"/></figure>
-                    <!-- /wp:image -->
+
+        <div class="row">
+            <?php
+                render_event_item(
+                    "http://localhost:8088/wp-content/uploads/2024/01/placeholder-event-1.jpg",
+                    "AMLD EPFL 2024",
+                    "23-26 Mar. 2024",
+                    "09:00-19:00",
+                    "EPFL, Lausanne",
+                    "Visit appliedmldays.org"
+                );
             
-                    <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-                    <div class="wp-block-group event-content">
-                        <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-                        <div class="wp-block-group">
-                            <header>
-                            <!-- wp:heading {"level":4} -->
-                            <h4>AMLD EPFL 2024</h4><br>
-                            <span class="event-date red">23-26 Mar. 2024</span>
-                            <!-- /wp:heading -->
-                                
-                            <!-- wp:list -->
-                            <ul class="event-time-location">
-                                <!-- wp:list-item -->
-                                <li class="event-time"><span>09:00-19:00</span></li>
-                                <!-- /wp:list-item -->
-                                <!-- wp:list-item -->
-                                <li class="event-location"><span>EPFL, Lausanne</span></li>
-                                <!-- /wp:list-item -->
-                            </ul>
-                            <!-- /wp:list -->
-                        </header>
-                        </div>
-                        <!-- /wp:group -->  
+                render_event_item(
+                    "http://localhost:8088/wp-content/uploads/2024/01/placeholder-event-2.jpg",
+                    "Industry Research Workshop: Machine Learning for Health Care and Life Sciences",
+                    "23-06 Dec. 2024",
+                    "09:00-19:00",
+                    "EPFL, Lausanne",
+                    "Visit epfl.ch"
+                );
 
-                        <!-- wp:group {"layout":{"type":"flex"}} -->
-				        <div class="wp-block-group link-sm">
-                            <span>Visit appliedmldays.org</span>
-                        </div>
-                        <!-- /wp:group -->
-                    </div>
-                    <!-- /wp:group -->  
-                </a>
-            </div>
-            <!-- /wp:group -->
-        
-            <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
-            <div class="wp-block-group col-sm-12 col-4">
-                <a class="wp-block-group event" href="#">
-                    <!-- wp:image {"id":18,"sizeSlug":"full","linkDestination":"none"} -->
-                    <figure class="wp-block-image"><img src="http://localhost:8088/wp-content/uploads/2024/01/placeholder-event-2.jpg" alt="" class="wp-image-18"/></figure>
-                    <!-- /wp:image -->
-            
-                    <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-                    <div class="wp-block-group event-content">
-                        <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-                        <div class="wp-block-group">
-                            <!-- wp:heading {"level":4} -->
-                            <h4>Industry Research Workshop: Machine Learning for Health Care and Life Sciences</h4><br>
-                            <span class="event-date red">23-26 Dec. 2024</span>
-                            <!-- /wp:heading -->
-                                
-                            <!-- wp:list -->
-                            <ul class="event-time-location">
-                                <!-- wp:list-item -->
-                                <li class="event-time"><span>09:00-19:00</span></li>
-                                <!-- /wp:list-item -->
-                                <!-- wp:list-item -->
-                                <li class="event-location"><span>EPFL, Lausanne</span></li>
-                                <!-- /wp:list-item -->
-                            </ul>
-                            <!-- /wp:list -->
-                        </div>
-                        <!-- /wp:group -->
-
-                        <!-- wp:group {"layout":{"type":"flex"}} -->
-				        <div class="wp-block-group link-sm">
-                            <span>Visit epfl.ch</span>
-                        </div>
-                        <!-- /wp:group -->
-                    </div>
-                    <!-- /wp:group -->  
-                </a>
-            </div>
-            <!-- /wp:group -->
-        
-            <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
-            <div class="wp-block-group col-sm-12 col-4">
-                <a class="wp-block-group event" href="#">
-                    <!-- wp:image {"id":18,"sizeSlug":"full","linkDestination":"none"} -->
-                    <figure class="wp-block-image"><img src="http://localhost:8088/wp-content/uploads/2024/01/placeholder-event-3.jpg" alt="" class="wp-image-18"/></figure>
-                    <!-- /wp:image -->
-            
-                    <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-                    <div class="wp-block-group event-content">
-                        <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-                        <div class="wp-block-group">
-                            <!-- wp:heading {"level":4} -->
-                            <h4>Python for Data Science and Machine Learning (EN) 2023</h4><br>
-                            <span class="event-date red">23-26 Mar. 2024</span>
-                            <!-- /wp:heading -->
-                                
-                            <!-- wp:list -->
-                            <ul class="event-time-location">
-                                <!-- wp:list-item -->
-                                <li class="event-time"><span>09:00-19:00</span></li>
-                                <!-- /wp:list-item -->
-                                <!-- wp:list-item -->
-                                <li class="event-location"><span>EPFL, Lausanne</span></li>
-                                <!-- /wp:list-item -->
-                            </ul>
-                            <!-- /wp:list -->
-                        </div>
-                        <!-- /wp:group --> 
-
-                        <!-- wp:group {"layout":{"type":"flex"}} -->
-				        <div class="wp-block-group link-sm">
-                            <span>Visit epfl.ch</span>
-                        </div>
-                        <!-- /wp:group -->
-                    </div>
-                    <!-- /wp:group -->  
-                </a>
-            </div>
-            <!-- /wp:group -->
-
+                render_event_item(
+                    "http://localhost:8088/wp-content/uploads/2024/01/placeholder-event-3.jpg",
+                    "Python for Data Science and Machine Learning (EN) 2023",
+                    "23-11 Dec. 2024",
+                    "09:00-19:00",
+                    "EPFL, Lausanne",
+                    "Visit epfl.ch"
+                );
+            ?>
         </div>
-        <!-- /wp:group -->
     </div>
-    <!-- /wp:group -->
 </div>
-<!-- /wp:group -->

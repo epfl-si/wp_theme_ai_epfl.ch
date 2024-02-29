4c3f77abd5c7595577393093f4d6e03c9570e80d
Ypso46 <gd.temp@pm.me>
2024-02-29 09:00:24 +0100

    [feature] add the loading of the phosphor icons in functions.php [refactor] change background image's name

--- functions.php
+++ functions.php
@@ -16,7 +16,10 @@ function theme_ai_center_register_pattern_categories() {
 }
 
 /*  DISABLE GUTENBERG STYLE IN HEADER| WordPress 5.9 */
-function theme_ai_center_deregister_styles() {
+function theme_ai_center_setup_styles() {
     wp_dequeue_style( 'global-styles' );
+	wp_enqueue_style( 'load-phosphor-icons', 'https://unpkg.com/@phosphor-icons/web@2.0.3/src/regular/style.css' );
 }
-add_action( 'wp_enqueue_scripts', 'theme_ai_center_deregister_styles', 100 );
+add_action( 'wp_enqueue_scripts', 'theme_ai_center_setup_styles', 100 );
+
+

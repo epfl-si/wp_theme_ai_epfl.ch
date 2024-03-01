b0f08e48cf16539ae950ff55f681dc51f10071d9
Ypso46 <gd.temp@pm.me>
2024-03-01 15:32:28 +0100

    [feature] get rid of the WP HTML comment

--- patterns/research-topics.php
+++ patterns/research-topics.php
@@ -5,38 +5,20 @@
  * Categories: featured, theme_ai_center/ai-center
  */
 ?>
-<!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group col-sm-12 col-md-8 col-3">
-    <!-- wp:group {"style":{"layout":{"selfStretch":"fixed","flexSize":"25%"}},"layout":{"type":"constrained"}} -->
-    <div class="wp-block-group sub-container-sm">
-        <!-- wp:heading {"level":6,"className":"h6"} -->
-        <h6 class="wp-block-heading h6">Research Topics</h6>
-        <!-- /wp:heading -->
-        
-        <!-- wp:list {"className":"divider-list"} -->
+<div class="col-sm-12 col-md-8 col-3">
+    <div class="sub-container-sm">
+        <h6 class="h6">Research Topics</h6>
+
         <ul class="divider-list">
-            <!-- wp:list-item -->
             <li>Mathematical and Statistical Foundations of Machine Learning</li>
-            <!-- /wp:list-item -->
             
-            <!-- wp:list-item -->
             <li>Interpretability and Exunstyledability</li>
-            <!-- /wp:list-item -->
             
-            <!-- wp:list-item -->
             <li>Safety, Reliability, and Robustness</li>
-            <!-- /wp:list-item -->
             
-            <!-- wp:list-item -->
             <li>Fairness and Bias of AI Systems</li>
-            <!-- /wp:list-item -->
             
-            <!-- wp:list-item -->
             <li>Machine Perception</li>
-            <!-- /wp:list-item -->
         </ul>
-        <!-- /wp:list -->
     </div>
-    <!-- /wp:group -->
 </div>
-<!-- /wp:group -->

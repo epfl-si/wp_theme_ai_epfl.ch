f5d217611da593f6d0954b218cd08b49c77aa77b
Ypso46 <gd.temp@pm.me>
2024-03-07 10:17:40 +0100

    [feature] the share buttons works with the method we found with Dominique

--- blocks/share-buttons/share-buttons.php
+++ blocks/share-buttons/share-buttons.php
@@ -1,5 +1,16 @@
 <?php
 
-// TODO: improve
+global $post;
+$posts = $query->posts;
 
-print(the_title());
+$post_url = urlencode(get_permalink($posts));
+$post_title = urlencode(get_the_title($posts));
+$facebook_url = 'https://www.facebook.com/sharer/sharer.php?u=' . $post_url;
+$linkedin_url = 'https://www.linkedin.com/shareArticle?mini=true&url=' . $post_url . '&title=' . $post_title;
+$twitter_url = 'https://twitter.com/intent/tweet?text=' . $post_title . '&url=' . $post_url;
+
+?>
+
+<li><a href="<?php echo($facebook_url); ?>"><i class="icon ph ph-facebook-logo" target="_blank"><span class="sr-only">Facebook</span></i></a></li>
+<li><a href="<?php echo($linkedin_url); ?>"><i class="icon ph ph-linkedin-logo" target="_blank"><span class="sr-only">LinkedIn</span></i></a></li>
+<li><a href="<?php echo($twitter_url); ?>"><i class="icon ph ph-twitter-logo" target="_blank"><span class="sr-only">Twitter</span></i></a></li>

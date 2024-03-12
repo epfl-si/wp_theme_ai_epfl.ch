68d9717a1dc5e82a910089b706ace2b8334067a7
Ypso46 <gd.temp@pm.me>
2024-03-12 16:31:15 +0100

    [feature] add a JS function for "copy to clipboard" in the Share section

--- blocks/share-buttons/share-buttons.php
+++ blocks/share-buttons/share-buttons.php
@@ -14,3 +14,4 @@ $twitter_url = 'https://twitter.com/intent/tweet?text=' . $post_title . '&url=' 
 <li><a href="<?php echo($facebook_url); ?>"><i class="icon ph ph-facebook-logo" target="_blank"><span class="sr-only">Facebook</span></i></a></li>
 <li><a href="<?php echo($linkedin_url); ?>"><i class="icon ph ph-linkedin-logo" target="_blank"><span class="sr-only">LinkedIn</span></i></a></li>
 <li><a href="<?php echo($twitter_url); ?>"><i class="icon ph ph-twitter-logo" target="_blank"><span class="sr-only">Twitter</span></i></a></li>
+<li><a href="#" id="copy-url-btn"><i class="icon ph ph-link-simple"><span class="sr-only">Copy link</span></i></a></li>

bb80b6aa82f78ac8f08fdf73ffbc331678264e41
Ypso46 <gd.temp@pm.me>
2024-03-18 16:03:38 +0100

    [feature] separate the file into two different parts

---
+++ patterns/approach.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: Approach
+ * Slug: theme_ai_center/approach
+ * Categories: featured, theme_ai_center/ai-center
+ */
+?>
+
+<div class="sub-container">
+    <h5 class="h6">Our Approach</h5>
+    <p>Delve into our unique methodology that merges interdisciplinary expertise with real-world applications.</p>
+    <a class="link" href="#"><span>Learn more</span></i></a>
+</div>

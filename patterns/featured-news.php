c8ce330db80b1d68e7a5fb9928516204bdb8535d
Ypso46 <gd.temp@pm.me>
2024-03-19 18:51:18 +0100

    [fix] restore an old working version by copy/pasting some code

--- patterns/featured-news.php
+++ patterns/featured-news.php
@@ -4,6 +4,44 @@
  * Slug: theme_ai_center/featuredNews
  * Categories: featured, theme_ai_center/ai-center
  */
+
+
+function render_news_item_lg ($title, $image_html, $paragraph_html, $read_more_html) {
+    ?>
+        <div class="col-sm-12 col-6">
+            <a class="news-item-lg">
+
+                <picture>
+                <?php echo($image_html); ?>
+                </picture>
+
+                <h3><?php echo($title); ?></h3>
+
+                <p><?php echo($paragraph_html); ?></p>
+
+                <div class="link">
+                <?php echo($read_more_html); ?>
+                </div>
+
+            </a>
+        </div>
+    <?php
+}
+function render_news_item ($title, $paragraph_html, $read_more_html) {
+    ?>
+        <a class="news-item">
+
+            <h5><?php echo($title); ?></h5>
+
+            <?php echo($paragraph_html); ?>
+
+            <div class="wp-block-group link-sm">
+            <?php echo($read_more_html); ?>
+            </div>
+        </a>
+    <?php
+}
+
 ?>
 
 
@@ -34,30 +72,60 @@
         </div>
 
         <div class="row">
-            <!-- wp:theme-ai-center/featured-news-lg /-->
-
             <?php
-                for($i = 0; $i < 2; $i++) {
+                $post = array_shift($posts);
+                if ($post) {
+                    render_news_item_lg (
+                        $post->post_title,
+                        get_the_post_thumbnail($post),
+                        get_the_excerpt($post),
+                        "<span>Read on epfl.ch</span>"
+                    );
+                } else {
                     ?>
-                    <div class="col-sm-12 col-3">
-                    <?php
-                        for($j = 0; $j < 3; $j++) {
-                            global $specific_excerpt_length;
-                            $specific_excerpt_length = 15;
-                            $post = array_shift($posts);
-                            ?>
-                            <!-- wp:theme-ai-center/featured-news /-->
-                            <?php
-                            unset($specific_excerpt_length);
-                        }
-                    ?>
-                    </div>
+                    <p>No posts here today!</p>
                     <?php
                 }
             ?>
-
-
             
+            <div class="col-sm-12 col-3">
+                <?php
+                    for($i = 0; $i < 3; $i++) {
+                            global $specific_excerpt_length;
+                            $specific_excerpt_length = 15;
+                            $post = array_shift($posts);
+                            render_news_item(
+                                $post->post_title,
+                                get_the_excerpt($post),
+                                "<span>Read on epfl.ch</span>"
+                            );
+                            ?>
+
+                            <?php
+                            unset($specific_excerpt_length);
+                    }
+                    
+                ?>
+            </div>
+            <div class="col-sm-12 col-3">
+                <?php
+                    for($i = 0; $i < 3; $i++) {
+                            global $specific_excerpt_length;
+                            $specific_excerpt_length = 15;
+                            $post = array_shift($posts);
+                            render_news_item(
+                                $post->post_title,
+                                get_the_excerpt($post),
+                                "<span>Read on epfl.ch</span>"
+                            );
+                            ?>
+
+                            <?php
+                            unset($specific_excerpt_length);
+                    }
+                    
+                ?>
+            </div>
         </div>
     </div>
 </div>

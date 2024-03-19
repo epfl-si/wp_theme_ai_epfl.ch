976af3e997e3d8a6b3387e6161e3a5469bce3406
Ypso46 <gd.temp@pm.me>
2024-03-19 16:15:08 +0100

    [feature] develop the function to get the page title dynamically, thanks to Rosa

--- patterns/hero.php
+++ patterns/hero.php
@@ -4,13 +4,24 @@
  * Slug: theme_ai_center/hero
  * Categories: featured, theme_ai_center/ai-center
  */
-?>
 
-<div class="hero">
-    <div class="center">
-        <div class="hero-content">
-          <h1>Pioneering Intelligence,<br>
-          Transforming Futures</h1>
+function render_news_item_lg ($title) {
+  ?>        
+    <div class="hero">
+        <div class="center">
+            <div class="hero-content">
+              <h1><?php echo($title); ?></h1>
+            </div>
         </div>
     </div>
-</div>
+  <?php
+}
+
+$uri = $_SERVER['REQUEST_URI'];
+$equalsPos = strpos($uri, '=');
+if ($equalsPos !== false) {
+  $pageId = substr($uri, $equalsPos + 1);
+ 
+  render_news_item_lg (get_the_title($pageId));
+} 
+         

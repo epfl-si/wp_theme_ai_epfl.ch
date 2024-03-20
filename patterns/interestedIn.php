f2e3032197e037b96080da5b5747782b67e7c6fb
Ypso46 <gd.temp@pm.me>
2024-03-20 09:58:30 +0100

    [feature] the interested in part is now retrieving the information dynamically

--- patterns/interestedIn.php
+++ patterns/interestedIn.php
@@ -4,16 +4,55 @@
  * Slug: theme_ai_center/interestedIn
  * Categories: featured, theme_ai_center/ai-center
  */
+
+
+function render_post_item ($link, $title, $paragraph_html, $read_more_html) {
 ?>
+    <a class="card" href="<?php echo($link); ?>">
 
-<div class="card-deck bg-grey-100">
-    <div class="center">
-        <div class="section-header">
-          <h6>You may also be interested in</h6>
+        <header>
+            <h4><?php echo($title) ?><br>
+        </header>
+        <div class="card-content">
+            <p><?php echo($paragraph_html) ?></p>
+            <div class="link-sm">
+                <span>
+                    <?php echo($read_more_hmtl) ?>
+                </span>
+                <i class="icon ph ph-arrow-up-right"></i>
+            </div>
+        </div>                                  
+    </a>
+<?php
+}
+
+
+$args = array(
+    'posts_per_page' => -1,
+    'orderby'   => array (
+        'date' =>'DESC'
+    )
+);
+
+$query = new WP_Query( $args );
+$posts = $query->posts;
+$post_title = $posts[0]->post_title;
+
+
+
+    
+    for($i = 0; $i < 3; $i++) {
+        ?>
+        <div class="col-sm-12 col-3">
+        <?php
+        $post = array_shift($posts);
+        render_post_item(
+            get_permalink($post),
+            $post->post_title,
+            get_the_excerpt($post),
+            "Read more"
+        );
+        ?>
         </div>
-
-        <div class="row">
-            <!-- wp:theme-ai-center/interested-in /-->            
-        </div>   
-    </div>
-</div>
+    <?php
+    }
